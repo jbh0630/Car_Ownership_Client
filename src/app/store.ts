786f032711c183f1dda065ca8747c791d90331fa
Byeonghyeok Jo <69543299+jbh0630@users.noreply.github.com>
2022-06-13 01:02:13 -0700

    edit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action, createReducer } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import peopleListsReducer from '../features/lists/ListsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     peopleLists: peopleListsReducer,
   },
 });
